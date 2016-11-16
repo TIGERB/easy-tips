8e0e97a435e7a308b9c2896f3c453f11223cd7ac
tigerb <624661874@qq.com>
2016-11-16 16:59:42 +0800

    feat(singleton): add pattern singleton

---
+++ singleton/singleton.php
@@ -0,0 +1,59 @@
+<?php
+namespace singleton;
+
+/**
+ * 单例
+ */
+class Singleton
+{
+  /**
+   * 自身实例
+   * 
+   * @var object
+   */
+  private static $_instance;
+
+  /**
+   * 构造函数
+   * 
+   * @return void
+   */
+  public function __contruct()
+  {
+    
+  }
+
+  /**
+   * 魔法方法
+   * 禁止clone对象
+   * 
+   * @return string
+   */
+  public function __clone()
+  {
+    echo 'clone is forbidden';
+  }
+
+  /**
+   * 获取实例
+   * 
+   * @return object
+   */
+  public static function getInstance()
+  {
+    if (!self::$_instance instanceof self) {
+      self::$_instance = new self;
+    }
+    return self::$_instance;
+  }
+
+  /**
+   * 测试方法
+   * 
+   * @return string
+   */
+  public function test()
+  {
+    echo "这是个测试 \n";
+  }
+}
---
+++ singleton/test.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * 创建型模式
+ * 
+ * php单例模式
+ * 
+ * @author  TIGERB <tigerbcode@gmail.com>
+ * @example 运行 php test.php
+ */
+
+
+// 注册自加载
+spl_autoload_register('autoload');
+
+function autoload($class)
+{
+  require dirname($_SERVER['SCRIPT_FILENAME']) . '//..//' . str_replace('\\', '/', $class) . '.php';
+}
+
+/************************************* test *************************************/
+
+use singleton\Singleton;
+
+// 获取单例
+$instance = Singleton::getInstance();
+$instance->test();
+
+// clone对象试试
+$instanceClone = clone $instance;
