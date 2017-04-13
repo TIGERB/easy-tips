84ee11e15e2796f041241b65572e8aaba5f3f1fc
Arthur Guo <jealone@gmail.com>
2017-04-13 15:02:42 +0800

    feature(patterns/proxy):common proxy

---
+++ patterns/proxy/common/Proxy.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace common;
+
+class Proxy implements Subject {
+
+    private $subject = null;
+
+    /**
+     * Proxy constructor.
+     */
+    public function __construct(Subject $_subject) {
+        $this->subject = $_subject;
+    }
+
+    public function doSomething() {
+        $this->subject->doSomething();
+    }
+
+}
