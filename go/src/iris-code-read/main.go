35b6e51e11d2c38bba24c18ff935d243f4ec1ee5
TIGERB <tigerbcode@gmail.com>
2019-07-14 15:37:27 +0800

    tip(go): add echo read

--- go/src/iris-code-read/main.go
+++ go/src/iris-code-read/main.go
@@ -1,13 +1,21 @@
 package main
 
-import "github.com/kataras/iris"
+import (
+	"fmt"
+
+	"github.com/kataras/iris"
+)
 
 func main() {
 	app := iris.Default()
 	app.Get("/ping", func(ctx iris.Context) {
+		fmt.Println("ping")
 		ctx.JSON(iris.Map{
 			"message": "pong",
 		})
+		ctx.Next()
+	}, func(ctx iris.Context) {
+		fmt.Println("ping2")
 	})
 	app.Run(iris.Addr(":8888"))
 }
