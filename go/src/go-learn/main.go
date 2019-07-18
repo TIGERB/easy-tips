641896d3298275e25d26140aaa5459abc3d2415b
TIGERB <tigerbcode@gmail.com>
2019-07-18 20:22:34 +0800

    tip(go/go-learn): chain

--- go/src/go-learn/main.go
+++ go/src/go-learn/main.go
@@ -2,33 +2,115 @@ package main
 
 import (
 	"fmt"
-	"sync"
-	"time"
 )
 
-// 导入net/http包
+// Context Context
+type Context struct {
+}
 
-func init() {
+// Handler Handler
+type Handler interface {
+	Do(c *Context) error
+	SetNext(h Handler) Handler
+	Run(c *Context)
+}
 
+// Next Next
+type Next struct {
+	nextHandler Handler
+}
+
+// SetNext SetNext
+func (n *Next) SetNext(h Handler) Handler {
+	n.nextHandler = h
+	return h
+}
+
+// Run Run
+func (n *Next) Run(c *Context) {
+	if n.nextHandler != nil {
+		(n.nextHandler).Do(c)
+		(n.nextHandler).Run(c)
+	}
+}
+
+// NullHandler NullHandler·
+type NullHandler struct {
+	Next
+}
+
+// Do Do
+func (h *NullHandler) Do(c *Context) error {
+	// do nothing...
+	return nil
+}
+
+// SignHandler SignHandler
+type SignHandler struct {
+	Next
+}
+
+// Do Do
+func (h *SignHandler) Do(c *Context) error {
+	fmt.Println("SignHandler")
+	return nil
+}
+
+// ArgumentsHandler ArgumentsHandler
+type ArgumentsHandler struct {
+	Next
+}
+
+// Do Do
+func (h *ArgumentsHandler) Do(c *Context) error {
+	fmt.Println("ArgumentsHandler")
+	return nil
+}
+
+// FrequentHandler FrequentHandler
+type FrequentHandler struct {
+	Next
+}
+
+// Do Do
+func (h *FrequentHandler) Do(c *Context) error {
+	fmt.Println("FrequentHandler")
+	return nil
+}
+
+// LogHandler LogHandler
+type LogHandler struct {
+	Next
+}
+
+// Do Do
+func (h *LogHandler) Do(c *Context) error {
+	fmt.Println("LogHandler")
+	return nil
 }
 
 func main() {
-	var wg sync.WaitGroup
+	nullHandler := &NullHandler{}
+	argumentsHandler := &ArgumentsHandler{}
+	signHandler := &SignHandler{}
+	frequentHandler := &FrequentHandler{}
 
-	// fmt.Println("waiting")
-	// // wg.Wait()
-	// fmt.Println("end")
+	nullHandler.SetNext(argumentsHandler).SetNext(signHandler).SetNext(frequentHandler)
+	nullHandler.Run(&Context{})
 
-	for index := 0; index < 10; index++ {
-		wg.Add(1)
-		go func(index int) {
-			time.Sleep(10 * time.Second)
-			fmt.Println(index)
-			wg.Done()
-		}(index)
+	fmt.Println("----------------------")
+
+	middlewares := make([]Handler, 0)
+	middlewares = append(middlewares, nullHandler)
+	middlewares = append(middlewares, argumentsHandler)
+	middlewares = append(middlewares, signHandler)
+	middlewares = append(middlewares, frequentHandler)
+
+	for k, handler := range middlewares {
+		if k == 0 {
+			continue
+		}
+		middlewares[k-1].SetNext(handler)
 	}
-
-	fmt.Println("waiting")
-	wg.Wait()
-	fmt.Println("end")
+	nullHandler.Run(&Context{})
 }
