a59c344677696128a17b3f5452bc35d490886227
TIGERB <tigerbcode@gmail.com>
2020-04-04 14:25:24 +0800

    tip(go/patterns): observer

--- go/src/patterns/observer/observer.go
+++ go/src/patterns/observer/observer.go
@@ -6,12 +6,61 @@ package main
 //@auhtor TIGERB<https://github.com/TIGERB>
 //------------------------------------------------------------
 
-import "fmt"
+import (
+	"fmt"
+	"reflect"
+	"runtime"
+)
 
-// ObserverOrderCreateInterface 定义一个订单创建观察者的接口
-type ObserverOrderCreateInterface interface {
+// Observable 被观察者
+type Observable interface {
+	Attach(observer ...ObserverInterface) Observable
+	Detach(observer ObserverInterface) Observable
+	Notify() error
+}
+
+// ObservableConcrete 一个具体的 创建订单的被观察者
+type ObservableConcrete struct {
+	observerList []ObserverInterface
+}
+
+// Attach 注册观察者
+// @param $observer ObserverInterface 观察者列表
+func (o *ObservableConcrete) Attach(observer ...ObserverInterface) Observable {
+	o.observerList = append(o.observerList, observer...)
+	return o
+}
+
+// Detach 注销观察者
+// @param $observer ObserverInterface 待注销的观察者
+func (o *ObservableConcrete) Detach(observer ObserverInterface) Observable {
+	if len(o.observerList) == 0 {
+		return o
+	}
+	for k, observerItem := range o.observerList {
+		if observer == observerItem {
+			fmt.Println(runFuncName(), "注销:", reflect.TypeOf(observer))
+			o.observerList = append(o.observerList[:k], o.observerList[k+1:]...)
+		}
+	}
+	return o
+}
+
+// Notify 通知观察者
+func (o *ObservableConcrete) Notify() (err error) {
+	// code ...
+	for _, observer := range o.observerList {
+		if err = observer.Do(o); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+// ObserverInterface 定义一个观察者的接口
+type ObserverInterface interface {
 	// 自身的业务
-	Do(Observable *ObservableOrderCreate) error
+	Do(o Observable) error
 }
 
 // Param 参数校验
@@ -19,122 +68,26 @@ type Param struct {
 }
 
 // Do 参数校验
-func (observer *Param) Do(Observable *ObservableOrderCreate) error {
+func (observer *Param) Do(o Observable) error {
 	// code...
 	fmt.Println("Param Code...")
-	return nil
-}
-
-// Address 地址校验等
-type Address struct {
-}
-
-// Do 地址校验等
-func (observer *Address) Do(Observable *ObservableOrderCreate) error {
-	// code...
-	fmt.Println("Address Code...")
-	return nil
-}
-
-// Check 其他校验
-type Check struct {
-}
-
-// Do 其他校验
-func (observer *Check) Do(Observable *ObservableOrderCreate) error {
-	// code...
-	fmt.Println("Check Code...")
-	return nil
-}
-
-// Order 写订单表等
-type Order struct {
-}
-
-// Do 写订单表等
-func (observer *Order) Do(Observable *ObservableOrderCreate) error {
-	// code...
-	fmt.Println("Order Code...")
-	return nil
-}
-
-// OrderItem 写订单商品信息表等
-type OrderItem struct {
-}
-
-// Do 写订单商品信息表等
-func (observer *OrderItem) Do(Observable *ObservableOrderCreate) error {
-	// code...
-	fmt.Println("OrderItem Code...")
-	return nil
-}
-
-// Log 日志
-type Log struct {
-}
-
-// Do 日志
-func (observer *Log) Do(Observable *ObservableOrderCreate) error {
-	// code...
-	fmt.Println("Log Code...")
-	return nil
-}
-
-// 等等观察者... 不再赘述
-
-// Request 请求数据对象
-type Request struct {
-}
-
-// Observable 被观察者
-type Observable interface {
-	Attach(observer ...ObserverOrderCreateInterface) *ObservableOrderCreate
-	Notify() error
-}
-
-// ObservableOrderCreate 一个具体的 创建订单的被观察者
-type ObservableOrderCreate struct {
-	InfoUser     interface{}
-	InfoProduct  interface{}
-	InfoAddress  interface{}
-	InfoCoupon   interface{}
-	observerList []ObserverOrderCreateInterface
-}
-
-// Init 根据请求参数、购物车数据 初始化机器人的属性
-func (Observable *ObservableOrderCreate) Init(r *Request) *ObservableOrderCreate {
-	// code ...
-	return Observable
-}
-
-// Attach 注册观察者
-// @param $observer ObserverOrderCreateInterface 观察者列表
-func (Observable *ObservableOrderCreate) Attach(observer ...ObserverOrderCreateInterface) *ObservableOrderCreate {
-	Observable.observerList = append(Observable.observerList, observer...)
-	return Observable
-}
-
-// Notify 通知观察者
-func (Observable *ObservableOrderCreate) Notify() error {
-	// code ...
-	for _, observer := range Observable.observerList {
-		observer.Do(Observable)
-	}
 	return nil
 }
 
 // 客户端调用
 func main() {
 	// 具体使用
-	(&ObservableOrderCreate{}).Init(&Request{}).
+	(&ObservableConcrete{}).
 		Attach( // 观察者观察被观察者
 			&Param{},
-			&Address{},
-			&Check{},
-			&Order{},
-			&OrderItem{},
-			&Address{},
-			&Log{},
 		).
 		Notify() // 被观察者通知观察者
+}
+
+// 获取正在运行的函数名
+func runFuncName() string {
+	pc := make([]uintptr, 1)
+	runtime.Callers(2, pc)
+	f := runtime.FuncForPC(pc[0])
+	return f.Name()
 }
