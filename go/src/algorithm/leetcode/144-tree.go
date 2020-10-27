890ecd020e3dd48a2ba54d7eca1f7ddc8cebfe04
TIGERB <tigerbcode@gmail.com>
2020-10-27 13:04:34 +0800

    tip(go/algorithm/leetcode): add 144-tree

---
+++ go/src/algorithm/leetcode/144-tree.go
@@ -0,0 +1,25 @@
+package leetcode
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func preorderTraversal(root *TreeNode) []int {
+	if root == nil {
+		return []int{}
+	}
+	res := []int{root.Val}
+	res = append(res, preorderTraversal(root.Left)...)
+	res = append(res, preorderTraversal(root.Right)...)
+	return res
+}
