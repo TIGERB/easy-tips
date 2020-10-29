26a976067dafc5e7b4a481b27a638ba369f6cb6a
TIGERB <tigerbcode@gmail.com>
2020-10-29 20:06:26 +0800

    tip(go/algorithm): tree unrecursion & stack

---
+++ go/src/algorithm/stack.go
@@ -0,0 +1,45 @@
+package main
+
+import "fmt"
+
+type stack struct {
+	val []interface{}
+}
+
+func New() *stack {
+	return &stack{val: []interface{}{}}
+}
+
+func (s *stack) Push(item interface{}) {
+	s.val = append(s.val, item)
+}
+
+func (s *stack) Pop() interface{} {
+	len := len(s.val)
+	if len == 0 {
+		return nil
+	}
+	item := s.val[len-1]
+	s.val = append(s.val[:0], s.val[:len-1]...)
+	return item
+}
+
+func (s *stack) Get() []interface{} {
+	return s.val
+}
+
+func (s *stack) Len() int {
+	return len(s.val)
+}
+
+func main() {
+	stack := New()
+	stack.Push(1)
+	stack.Push(2)
+	stack.Push(3)
+	fmt.Println(stack.Get(), stack.Len())
+	stack.Push(6)
+	stack.Push(9)
+	fmt.Println(stack.Get(), stack.Len())
+	fmt.Println(stack.Pop(), stack.Get(), stack.Len())
+}
