25da062c988cc347c5cb4bbfaec6ab0bb5f6703d
TIGERB <tigerbcode@gmail.com>
2021-12-11 21:53:28 +0800

    tip(go/go-learn): up

--- go/go-learn/main.go
+++ go/go-learn/main.go
@@ -1,31 +1,35 @@
 package main
 
 import (
-	"fmt"
-	"unsafe"
+	// 导入net/http包
+	"net/http"
+	"os"
+	"runtime/trace"
 )
 
-type flightGroup interface {
-	// Done is called when Do is done.
-	Do(key string, fn func() (interface{}, error)) (interface{}, error)
-}
-
-// golangci-lint run --disable-all -E maligned
-
-type Demo struct {
-	A int8
-	B int64
-	C int8
-}
-
-type DemoTwo struct {
-	A int8
-	C int8
-	B int64
-}
-
 func main() {
-	demo := Demo{}
-	demoTwo := DemoTwo{}
-	fmt.Println(unsafe.Sizeof(demo), unsafe.Sizeof(demoTwo))
+	//创建trace文件
+	f, err := os.Create("trace.out")
+	if err != nil {
+		panic(err)
+	}
+
+	defer f.Close()
+
+	//启动trace goroutine
+	err = trace.Start(f)
+	if err != nil {
+		panic(err)
+	}
+	defer trace.Stop()
+
+	// ------------------ 使用http包启动一个http服务 方式一 ------------------
+	// *http.Request http请求内容实例的指针
+	// http.ResponseWriter 写http响应内容的实例
+	http.HandleFunc("/v1/demo", func(w http.ResponseWriter, r *http.Request) {
+		// 写入响应内容
+		w.Write([]byte("Hello TIGERB !\n"))
+	})
+	// 启动一个http服务并监听8888端口 这里第二个参数可以指定handler
+	http.ListenAndServe(":8888", nil)
 }
